4481f86302e32590159a101189caf15a6eb10f08
Administrator <iamsunli155@gmail.com>
2023-02-06 15:07:42 -0300

    .

--- src/D1021.java
+++ src/D1021.java
@@ -1,19 +1,30 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class D1021 {
     public static void main(String[] args) {
 
+        Locale.setDefault(Locale.US);
+
         Scanner sc = new Scanner(System.in);
-        int dias = sc.nextInt();
-        int ano,mes,dia;
+        double valor = sc.nextDouble();
+        double vetor[] = {100, 50, 20, 10, 5, 2, 1, 0.50, 0.25, 0.10, 0.05, 0.01};
+        double cedula, resto;
 
-        ano = dias / 365;
-        mes = (dias % 365) / 30;
-        dia = (dias % 365) % 30;
-
-        System.out.printf("%d ano(s)%n%d mes(es)%n%d dia(s)%n",ano, mes, dia);
-
-
+        System.out.println("NOTAS:");
+        resto = valor;
+        for (int i = 0;i <= 5;i++){
+            cedula = resto / vetor[i];
+            System.out.printf("%d nota(s) de R$ %.2f%n",(int) cedula, vetor[i]);
+            resto = resto % vetor[i];
+        }
+        System.out.println("MOEDAS:");
+        for (int i = 6;i < 12;i++){
+            cedula = resto / vetor[i];
+            cedula = (int) cedula;
+            System.out.printf("%d moeda(s) de R$ %.2f%n", (int) cedula, vetor[i]);
+            resto = resto % vetor[i];
+        }
         sc.close();
     }
 
