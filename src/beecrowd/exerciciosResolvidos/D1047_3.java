8512b4ec3939c32201c019af74c8ff666e0a22e1
Cássio Tiago <101597036+slnntk@users.noreply.github.com>
2023-03-28 15:25:38 -0300

    D1047 - OO
    new way to do the question.

---
+++ src/beecrowd/exerciciosResolvidos/D1047_3.java
@@ -0,0 +1,67 @@
+package beecrowd.exerciciosResolvidos;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+public class D1047_3 {
+    public static void main(String[] args) {
+        new JogoM();
+    }
+}
+
+class JogoM extends Hora{
+
+    private Date start;
+    private Date end;
+
+
+    public JogoM() {
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+        Scanner sc = new Scanner(System.in);
+        start = new Hora(sc.nextInt(), sc.nextInt()).getDate();
+        end = new Hora(sc.nextInt(), sc.nextInt()).getDate();
+
+        duration();
+    }
+
+    public void duration() {
+        int minutostotais = (int) TimeUnit.MINUTES.convert(end.getTime() - start.getTime(), TimeUnit.MILLISECONDS);
+        if (minutostotais <= 0) {
+            minutostotais += 1440;
+        }
+        System.out.printf("O JOGO DUROU %d HORA(S) E %d MINUTO(S)%n", minutostotais/60,minutostotais%60);
+
+    }
+}
+
+class Hora{
+
+    private int hora;
+    private int minuto;
+
+    private Date date;
+
+    public Hora() {
+    }
+
+    public Hora(int hora, int minuto){
+        this.hora = hora;
+        this.minuto = minuto;
+        this.date = hourAndMinut(minuto, hora);
+    }
+
+    public Date hourAndMinut(int minito, int hora){
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.HOUR_OF_DAY, hora);
+        cal.set(Calendar.MINUTE, minito);
+
+        return cal.getTime();
+    }
+
+    public Date getDate() {
+        return date;
+    }
+}
