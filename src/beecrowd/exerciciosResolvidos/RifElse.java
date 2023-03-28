b5767fa1aedd16e30b586bae8db79ce12f2b3db5
Cássio Tiago <101597036+slnntk@users.noreply.github.com>
2023-03-28 16:30:44 -0300

    ifElse-hackrank

--- src/beecrowd/exerciciosResolvidos/RifElse.java
+++ src/beecrowd/exerciciosResolvidos/RifElse.java
@@ -6,28 +6,16 @@ public class RifElse {
         public static void main(String[] args) {
                 Scanner sc = new Scanner(System.in);
                 int n = sc.nextInt();
-
-                while (n-- > 0) {
-                        int[] pts = {0, 0};
-                        for (int i = 0; i < 3; i++) {
-                                int x = sc.nextInt();
-                                int d = sc.nextInt();
-                                pts[0] += x * d;
+                sc.close();
+                if (n % 2 != 0 ){
+                        System.out.println("Weird");
+                } else{
+                        if (n > 20 || n >= 2 && n <= 5){
+                                System.out.println("Not Weird");
                         }
-
-                        for (int i = 0; i < 3; i++) {
-                                int x = sc.nextInt();
-                                int d = sc.nextInt();
-                                pts[1] += x * d;
-                        }
-
-                        if (pts[0] > pts[1]) {
-                                System.out.println("JOAO");
-                        } else {
-                                System.out.println("MARIA");
+                        else{
+                                System.out.println("Weird");
                         }
                 }
-
-                sc.close();
         }
 }
