201a01b00b59330c60e69380ff09fed482bf5589
Cássio Tiago <101597036+slnntk@users.noreply.github.com>
2023-03-31 09:40:35 -0300

    Update D1061.java

--- src/rascunhos/D1061.java
+++ src/rascunhos/D1061.java
@@ -4,16 +4,16 @@ import java.util.Scanner;
 
 public class D1061 {
     public static void main(String[] args) {
-        new PulaSapo();
+        new Jogo();
     }
 }
 
-class PulaSapo{
-    public PulaSapo() {
+class Jogo{
+    public Jogo() {
         Scanner sc = new Scanner(System.in);
         Sapo s = new Sapo(sc.nextInt());
         int n = sc.nextInt();
-        result(gameFinish(pularCanos(s.getSaltoMax(), sc, n)));
+        result(gameFinish(s.pularCanos(sc, n)));
         sc.close();
     }
 
@@ -32,28 +32,6 @@ class PulaSapo{
         return temp;
     }
 
-    public boolean pularCanos(int puloSapo, Scanner sc, int n){
-        boolean jumping = true;
-        Cano anterior = new Cano(sc.nextInt());
-        for (int i = 1;i < n;i++){
-            Cano c = new Cano(sc.nextInt());
-            if (!canJump(anterior.getAltura(), c.getAltura(), puloSapo)){
-                jumping = false;
-                break;
-            }
-            anterior = c;
-        }
-        return jumping;
-    }
-
-    public boolean canJump(int anterior, int cano, int puloSapo){
-        if (Math.abs(cano - anterior) <= puloSapo){
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
 }
 
 class ObjetoDoJogo {
@@ -71,11 +49,34 @@ class ObjetoDoJogo {
 
 
 class Sapo extends ObjetoDoJogo {
-    private int saltoMax;
+    private final int saltoMax;
 
     public Sapo(int saltoMax) {
         super(0);
         this.saltoMax = saltoMax;
+    }
+
+    public boolean pularCanos(Scanner sc, int n){
+        boolean jumping = true;
+        Cano anterior = new Cano(sc.nextInt());
+        for (int i = 1;i < n;i++){
+            Cano c = new Cano(sc.nextInt());
+            if (!canJump(anterior.getAltura(), c.getAltura())){
+                jumping = false;
+                break;
+            }
+            anterior = c;
+        }
+        return jumping;
+    }
+
+    public boolean canJump(int anterior, int cano){
+        if (Math.abs(cano - anterior) <= saltoMax){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
     public int getSaltoMax() {
