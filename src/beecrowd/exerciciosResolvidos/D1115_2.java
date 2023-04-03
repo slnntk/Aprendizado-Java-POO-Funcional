3504b1167c59323e553c0a1e47d27225cef72876
Cássio Tiago <101597036+slnntk@users.noreply.github.com>
2023-04-03 15:21:08 -0300

    D1115 - OO

--- src/beecrowd/exerciciosResolvidos/D1115_2.java
+++ src/beecrowd/exerciciosResolvidos/D1115_2.java
@@ -1,63 +1,82 @@
 package beecrowd.exerciciosResolvidos;
 
+import java.util.Scanner;
+
 public class D1115_2 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        new Coordenada(sc);
     }
 }
 
-class Coordenada{
+class Coordenada extends Ponto{
 
-    private Ponto X;
-    private Ponto Y;
+    private final Ponto X;
+    private final Ponto Y;
 
-    public Coordenada(int x, int y) {
-        X = new Ponto(x);
-        Y = new Ponto(y);
+    public Coordenada(Scanner sc) {
+        super();
+        this.X  = new Ponto(sc.nextInt());
+        this.Y  = new Ponto(sc.nextInt());
+        System.out.println(getQuadrante());
     }
 
     public Ponto getX() {
         return X;
     }
 
-    public void setX(Ponto x) {
-        X = x;
+    public String getQuadrante() {
+        return new Quadrante(X.getNumberPoint(), Y.getNumberPoint()).getNomeQuadrante();
     }
 
-    public Ponto getY() {
-        return Y;
-    }
-
-    public void setY(Ponto y) {
-        Y = y;
-    }
 }
 
-class Ponto extends Coordenada{
+class Ponto{
 
     private int numberPoint;
 
-    public Ponto(int x, int y, int numberPoint) {
-        super(x, y);
+    public Ponto() {
+
+    }
+
+    public Ponto(int numberPoint) {
         this.numberPoint = numberPoint;
     }
 
     public int getNumberPoint() {
         return numberPoint;
     }
-
-    public void setNumberPoint(int numberPoint) {
-        this.numberPoint = numberPoint;
-    }
 }
 
 class Quadrante{
 
+    private String nomeQuadrante;
     private int x;
     private int y;
 
+    public Quadrante(int x, int y) {
+        this.x = x;
+        this.y = y;
+        searchQuadrante();
+    }
 
-    
+    public void searchQuadrante() {
+        if (x > 0 && y > 0) {
+            this.nomeQuadrante = "primeiro";
+        } else if (x < 0 && y > 0) {
+            this.nomeQuadrante = "segundo";
+        } else if (x < 0 && y < 0) {
+            this.nomeQuadrante = "terceiro";
+        } else if (x > 0 && y < 0) {
+            this.nomeQuadrante = "quarto";
+        } else {
+            this.nomeQuadrante = "origem";
+        }
+    }
+
+    public String getNomeQuadrante() {
+        return nomeQuadrante;
+    }
 }
 
 
