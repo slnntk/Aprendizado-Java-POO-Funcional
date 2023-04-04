3ce1bdb9ed2397a70acf05ba0d94b2e67e1e9a50
Cássio Tiago <101597036+slnntk@users.noreply.github.com>
2023-04-04 11:55:33 -0300

    Update Main.java
    ops, hackerrank question

--- src/rascunhos/Main.java
+++ src/rascunhos/Main.java
@@ -5,5 +5,19 @@ import java.util.Scanner;
 
 public class Main {
 
-     bv
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            if (x > 0 && y > 0){
+                System.out.println(x*y);
+            }
+            else{
+                throw new IllegalArgumentException("Breadth and height must be positive");
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println("java.lang.Exception: " + e.getMessage());
+        }
+    }
 }
