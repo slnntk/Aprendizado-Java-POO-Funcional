276cc1304e285b5bfdf88edae97c56ffa89523aa
Cássio Tiago <101597036+slnntk@users.noreply.github.com>
2023-04-04 13:55:06 -0300

    Update Main.java

--- src/cursopoo/CURSOJAVA/Arquivos/src/arquivosLeituraBasica/Main.java
+++ src/cursopoo/CURSOJAVA/Arquivos/src/arquivosLeituraBasica/Main.java
@@ -1,6 +1,7 @@
 package arquivosLeituraBasica;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -8,14 +9,15 @@ public class Main {
     public static void main(String[] args) {
         File file = new File("D:\\in.txt");
         Scanner sc = null;
-        try {
+        try{
             sc = new Scanner(file);
             while (sc.hasNextLine()){
                 System.out.println(sc.nextLine());
             }
-        } catch (IOException e) {
+        } catch (IOException e){
             System.out.println("Error: " + e.getMessage());
-        } finally {
+        }
+        finally {
             if (sc != null){
                 sc.close();
             }
