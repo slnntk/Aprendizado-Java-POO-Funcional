abf520364f42558481e203a2fe3a491106998413
Cássio Tiago <101597036+slnntk@users.noreply.github.com>
2023-04-04 14:37:23 -0300

    files

---
+++ src/cursopoo/CURSOJAVA/Arquivos/src/arquivosFileReaderBuffered/Main.java
@@ -0,0 +1,38 @@
+package arquivosFileReaderBuffered;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        String path = "D:\\in.txt";
+        FileReader fr = null;
+        BufferedReader br = null;
+        try{
+            fr = new FileReader(path);
+            br = new BufferedReader(fr);
+
+            String line = br.readLine();
+            while (line != null){
+                System.out.println(line);
+                line = br.readLine();
+            }
+        } catch (IOException e){
+            System.out.println("Error: " + e.getMessage());
+        } finally {
+
+            try{
+                if (br!=null){
+                    br.close();
+                }
+                if (fr != null){
+                    fr.close();
+                }
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
--- src/rascunhos/Main.java
+++ src/rascunhos/Main.java
@@ -1,23 +1,80 @@
 package rascunhos;
 
-import java.io.Serial;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-public class Main {
+public class Main{
+    public static void main(String[]args) throws IOException {
+            new CancelaEletronica();
+            }
+            }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        try {
-            int x = sc.nextInt();
-            int y = sc.nextInt();
-            if (x > 0 && y > 0){
-                System.out.println(x*y);
+    abstract class Transportes {
+    private int qtdPessoas;
+    private int qtdTransportes;
+
+    public int getQtdPessoas(){
+            return qtdPessoas;
             }
-            else{
-                throw new IllegalArgumentException("Breadth and height must be positive");
+
+    public void setQtdPessoas(int pessoas){
+            this.qtdPessoas+=pessoas;
             }
-        } catch (IllegalArgumentException e) {
-            System.out.println("java.lang.Exception: " + e.getMessage());
-        }
-    }
-}
+
+    public int getQtdTransportes(){
+            return qtdTransportes;
+            }
+
+    public void setQtdTransportes(int transportes){
+            this.qtdTransportes+=transportes;
+            }
+            }
+
+    class CancelaEletronica extends Transportes{
+
+    public CancelaEletronica() throws IOException {
+            BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+            String linha;
+
+            try{
+            while((linha=br.readLine())!=null){
+            String[]partes=linha.split(" ");
+            String comando=partes[0];
+            int n=Integer.parseInt(partes[1]);
+
+            if(comando.equals("ABEND")){
+            break;
+            }else if(comando.equals("VUELTA")){
+            entrada(n);
+            }else if(comando.equals("SALIDA")){
+            saida(n);
+            }
+            }
+            relatorio(getQtdPessoas(),getQtdTransportes());
+            }catch(IOException e){
+            e.printStackTrace();
+            }finally{
+            try{
+            br.close();
+            }catch(IOException e){
+            e.printStackTrace();
+            }
+            }
+            }
+
+    public void relatorio(int pessoas,int transportes){
+            System.out.println(Math.abs(pessoas));
+            System.out.println(Math.abs(transportes));
+            }
+
+    public void entrada(int pessoas){
+            this.setQtdPessoas(pessoas);
+            this.setQtdTransportes(1);
+            }
+
+    public void saida(int pessoas){
+            this.setQtdPessoas(-pessoas);
+            this.setQtdTransportes(-1);
+            }
+            }
