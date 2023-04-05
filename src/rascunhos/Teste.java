0ffbdcad57e12a33afb4f7326b39af70cc0124f3
Cássio Tiago <101597036+slnntk@users.noreply.github.com>
2023-04-05 15:02:05 -0300

    new learns
    list folders
    list files
    create a folder

--- src/rascunhos/Teste.java
+++ src/rascunhos/Teste.java
@@ -7,14 +7,34 @@ public class Teste {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        /**/
         System.out.println("Enter a folder path: ");
         String strPath = sc.nextLine();
-
         File path = new File(strPath);
-        File[] folders = path.listFiles(File::isDirectory);
+
+        /**/
+        System.out.println();
         System.out.println("FOLDERS:");
+        File[] folders = path.listFiles(File::isDirectory);
         for (File folder : folders){
             System.out.println(folder);
         }
+
+        /**/
+        System.out.println();
+        System.out.println("FILES:");
+        File[] files = path.listFiles(File::isFile);
+        for (File file : files){
+            System.out.println(file);
+        }
+
+        /**/
+        boolean sucess = new File(strPath + "\\subdir").mkdir();
+        if (sucess){
+            System.out.println("Directory created sucessfully");
+        }else{
+            System.out.println("Directory isn't created sucessfully");
+        }
+
     }
 }
