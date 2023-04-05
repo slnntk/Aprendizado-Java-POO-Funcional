f78b95d223f4153bc6fb2f43243cc4081614f959
Cássio Tiago <101597036+slnntk@users.noreply.github.com>
2023-04-05 13:35:22 -0300

    create a file reader and bufferedreader with resources

--- src/cursopoo/CURSOJAVA/Arquivos/src/arquivosFileReaderBufferedWithoutResources/Main.java
+++ src/cursopoo/CURSOJAVA/Arquivos/src/arquivosFileReaderBufferedWithoutResources/Main.java
@@ -1,9 +1,8 @@
-package arquivosFileReaderBuffered;
+package arquivosFileReaderBufferedWithoutResources;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
