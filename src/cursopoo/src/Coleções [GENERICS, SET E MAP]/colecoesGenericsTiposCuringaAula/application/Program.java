bdf6a44fa836925be7ddbd7da02da73626035f5a
Cássio Tiago <101597036+slnntk@users.noreply.github.com>
2023-04-12 16:08:23 -0300

    printing one list joker
    using the joker type to printing a typed list.

---
+++ src/cursopoo/src/Coleções [GENERICS, SET E MAP]/colecoesGenericsTiposCuringaAula/application/Program.java
@@ -0,0 +1,21 @@
+package colecoesGenericsTiposCuringaAula.application;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Program {
+    public static void main(String[] args) {
+
+        List<Integer> integerList = Arrays.asList(5, 2, 10);
+        printList(integerList);
+    }
+
+    public static void printList(List<?> list){
+
+        for (Object object : list){
+            System.out.println(object);
+        }
+
+    }
+
+}
