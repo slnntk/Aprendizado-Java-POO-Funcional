af92d7d77f365787ace5be0a61810d4537cf7240
Cássio Tiago <101597036+slnntk@users.noreply.github.com>
2023-04-14 09:25:30 -0700

    Update Program.java

--- src/cursopoo/src/Coleções [GENERICS, SET E MAP]/colecoesGenericsTiposCuringaAula/application/Program.java
+++ src/cursopoo/src/Coleções [GENERICS, SET E MAP]/colecoesGenericsTiposCuringaAula/application/Program.java
@@ -1,18 +1,13 @@
 package colecoesGenericsTiposCuringaAula.application;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-//supertipo de qualquer tipo de lista
 
 public class Program {
     public static void main(String[] args) {
 
-        List<Integer> integerList = new ArrayList<>();
-        integerList.add(1);
-        integerList.add(2);
-        integerList.add(200);
+        List<Integer> integerList = Arrays.asList(5, 2, 10);
         printList(integerList);
 
     }
