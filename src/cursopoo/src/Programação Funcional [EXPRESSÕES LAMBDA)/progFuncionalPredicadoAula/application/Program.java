a4589757ae29fe06292a25442f3b8fec348b787f
Cássio Tiago <101597036+slnntk@users.noreply.github.com>
2023-04-18 15:25:04 -0300

    predicate

--- src/cursopoo/src/Programação Funcional [EXPRESSÕES LAMBDA)/progFuncionalPredicadoAula/application/Program.java
+++ src/cursopoo/src/Programação Funcional [EXPRESSÕES LAMBDA)/progFuncionalPredicadoAula/application/Program.java
@@ -1,15 +1,15 @@
-package progFuncionalCostumerAula.application;
+package progFuncionalPredicadoAula.application;
 
 import progFuncionalCostumerAula.entities.Product;
-import progFuncionalCostumerAula.util.PriceUpdate;
+import progFuncionalPredicadoAula.util.ProductPredicate;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Consumer;
+import java.util.Locale;
 
 public class Program {
-
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
 
         List<Product> list = new ArrayList<>();
 
@@ -17,12 +17,9 @@ public class Program {
         list.add(new Product("Mouse", 50.00));
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
+        /* remover todo produto p tal que p.getPrice seja maior ou igual a 100 */
+        list.removeIf(p -> p.getPrice() >= 100);
 
-        double factor = 1.1;
-
-       Consumer<Product> productConsumer = p -> p.setPrice(p.getPrice()*factor);;
-       list.forEach(Product::staticPriceUpdate);
-       list.forEach(System.out::println);
+        list.forEach(System.out::println);
     }
-
 }
---
+++ src/cursopoo/src/Programação Funcional [EXPRESSÕES LAMBDA)/progFuncionalPredicadoAula/entities/Product.java
@@ -0,0 +1,25 @@
+package progFuncionalPredicadoAula.entities;
+
+public class Product {
+
+    private String name;
+    private Double price;
+
+    public Product(String name, Double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return name+","+String.format("%.2f", price);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+}
