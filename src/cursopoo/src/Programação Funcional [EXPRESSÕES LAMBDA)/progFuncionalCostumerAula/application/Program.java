66048bf9e13620c97193f0dac65570846de7e5d8
Cássio Tiago <101597036+slnntk@users.noreply.github.com>
2023-04-18 14:56:51 -0300

    Update Program.java

--- src/cursopoo/src/Programação Funcional [EXPRESSÕES LAMBDA)/progFuncionalCostumerAula/application/Program.java
+++ src/cursopoo/src/Programação Funcional [EXPRESSÕES LAMBDA)/progFuncionalCostumerAula/application/Program.java
@@ -17,12 +17,11 @@ public class Program {
         list.add(new Product("Mouse", 50.00));
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
+        
+        double factor = 1.1;
 
-        Consumer<Product> productConsumer = p -> p.setPrice(p.getPrice()*factor);;
+       Consumer<Product> productConsumer = p -> p.setPrice(p.getPrice()*factor);;
        list.forEach(Product::staticPriceUpdate);
-
-       double factor = 1.1;
-
        list.forEach(System.out::println);
     }
 
