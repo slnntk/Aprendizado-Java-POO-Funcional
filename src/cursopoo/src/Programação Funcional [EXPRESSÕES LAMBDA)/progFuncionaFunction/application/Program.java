b0553b1a51c88bb492f3ec671e28a93b5d00b921
Cássio Tiago <101597036+slnntk@users.noreply.github.com>
2023-04-24 15:33:24 -0300

    function

--- src/cursopoo/src/Programação Funcional [EXPRESSÕES LAMBDA)/progFuncionaFunction/application/Program.java
+++ src/cursopoo/src/Programação Funcional [EXPRESSÕES LAMBDA)/progFuncionaFunction/application/Program.java
@@ -1,23 +1,28 @@
 package progFuncionaFunction.application;
 
+import progFuncionaFunction.util.UpperCaseName;
 import progFuncionalCostumerAula.entities.Product;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Program {
     public static void main(String[] args) {
 
         List<Product> list = new ArrayList<>();
-
         list.add(new Product("Tv", 900.00));
         list.add(new Product("Mouse", 50.00));
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
 
-       //list.forEach(p -> p.setName(p.getName().toUpperCase()));
-        list.forEach(System.out::println);
+        Function<Product, String> prod = p -> p.getName().toUpperCase();
+        //List<String> names = list.stream().map(prod).collect(Collectors.toList());
 
+
+        List<String> names = list.stream().map(p -> p.getName().toUpperCase()).collect(Collectors.toList());
+        names.forEach(System.out::println);
     }
 
 
