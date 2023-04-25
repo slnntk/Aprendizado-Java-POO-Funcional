7b485ca6b0a678abd6bddd7dacff3e4735c832ff
Cássio Tiago <101597036+slnntk@users.noreply.github.com>
2023-04-25 15:14:22 -0300

    Update Program.java

--- src/rascunhos/Func/application/Program.java
+++ src/rascunhos/Func/application/Program.java
@@ -1,22 +1,20 @@
 package rascunhos.Func.application;
 
 import rascunhos.Func.entities.Product;
-import rascunhos.Func.util.ProductService;
 
 import java.io.*;
+import java.util.Comparator;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
-
 public class Program {
     public static void main(String[] args) {
 
         Set<Product> productSet = new HashSet<>();
-        ProductService ps = new ProductService();
 
         String path = "C:\\Users\\SLNNTK\\Documents\\T";
         String in = "\\TESTEPRODUCTIN.txt";
-        String out = "\\TESTEPRODUCTOUT.txt";
+        String out = "\\TESTEPRODUCTOUT2.txt";
 
         try (BufferedReader br = new BufferedReader(new FileReader(path+in))){
             String line = br.readLine();
@@ -28,11 +26,25 @@ public class Program {
         } catch (FileException | IOException e) {
             throw new FileException("Error in read the file");
         }
-        Set<Product> productList = productSet.stream().filter(product -> product.getName().charAt(0) == 'T').collect(Collectors.toSet());
-        System.out.println(productList);
+
+        Comparator<String> comparator = (x1, x2) -> x1.toUpperCase().compareTo(x2.toUpperCase());
+
+
+        double avg = productSet.stream()
+                    .map(x -> x.getPrice())
+                    .reduce(0.0, (x, y) -> x+y/productSet.size());
+
+        List<String> stringSet = productSet.stream()
+                    .filter(x -> x.getPrice() < avg)
+                    .map(x -> x.getName())
+                    .sorted(comparator.reversed()).toList();
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(path+out))){
-            double sumResult = ps.filteredSum(productSet, p -> p.getPrice() > 0);
-            bw.write(String.valueOf(sumResult));
+            bw.write("Media: " + String.format("%.2f", avg));
+            bw.newLine();
+            for (String s : stringSet){
+                bw.write(s);
+                bw.newLine();
+            }
         } catch (FileException | IOException e) {
             throw new FileException("Error in write file");
         }
