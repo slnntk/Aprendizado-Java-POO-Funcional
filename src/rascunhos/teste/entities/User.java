9113fc2a2c4bcf13d3f48080ab6397b87e379cde
Cássio Tiago <101597036+slnntk@users.noreply.github.com>
2023-05-08 15:41:39 -0300

    Update User.java

--- src/rascunhos/teste/entities/User.java
+++ src/rascunhos/teste/entities/User.java
@@ -1,5 +1,7 @@
 package rascunhos.teste.entities;
 
+import rascunhos.teste.interfaces.Pessoa;
+
 public class User extends Pessoa implements Comparable<Pessoa>{
     public User(String name, String email, Double baseSalary) {
         super(name, email, baseSalary);
