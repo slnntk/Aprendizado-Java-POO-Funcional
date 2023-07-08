9bbf35220e7197a2c69d8622cb2ff119a5c63e17
Cássio Tiago <101597036+slnntk@users.noreply.github.com>
2023-07-07 22:50:42 -0300

    organization

--- src/a/Main.java
+++ src/a/Main.java
@@ -5,26 +5,14 @@ import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
-
         Locale.setDefault(Locale.US);
 
-        Leitor a = new Leitor();
-        List<Item> listEmp = a.lerArchiveEmp("D:\\Users\\cassi\\Downloads-1\\tsv\\EMP2.tsv");
-        List<Item> listMLG = a.lerArchiveMlg("D:\\Users\\cassi\\Downloads-1\\tsv\\MLG2.tsv");
-        compararListas(listEmp, listMLG);
+        Leitor leitor = new Leitor();
+        List<Item> listEmp = leitor.lerArchiveEmp("D:\\Users\\cassi\\Downloads-1\\tsv\\EMP4.tsv");
+        List<Item> listMLG = leitor.lerArchiveMlg("D:\\Users\\cassi\\Downloads-1\\tsv\\MLG4.tsv");
+        Verificador verificador = new Verificador();
+        verificador.compararListas(listEmp, listMLG);
     }
 
-    public static void compararListas(List<Item> emp, List<Item> mlg) {
-        for (Item itemEmpresa : emp) {
-            for (Item itemMLG : mlg) {
-                if (itemEmpresa.getItem().equals(itemMLG.getItem())) {
-                    if (itemMLG.getValorUnitario() > itemEmpresa.getValorUnitario()){
-                        System.out.println("MLG: " + itemMLG.getItem()+ ": " + itemMLG.getValorUnitario());
-                        System.out.println("IFCE: " + itemEmpresa.getItem() + ": " + itemEmpresa.getValorUnitario());
-                        System.out.println("--------------------------------------------------------------------");
-                    }
-                }
-            }
-        }
-    }
+
 }
---
+++ src/a/Verificador.java
@@ -0,0 +1,28 @@
+package a;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Verificador {
+
+    public void compararListas(List<Item> emp, List<Item> mlg) {
+        Set<String> mlgItems = new HashSet<>();
+        for (Item itemMLG : mlg) {
+            mlgItems.add(itemMLG.getItem());
+        }
+
+        for (Item itemEmpresa : emp) {
+            if (mlgItems.contains(itemEmpresa.getItem())) {
+                for (Item itemMLG : mlg) {
+                    if (itemEmpresa.getItem().equals(itemMLG.getItem()) && itemMLG.getValorUnitario() > itemEmpresa.getValorUnitario()) {
+                        System.out.println("MLG: " + itemMLG.getItem()+ ": " + itemMLG.getValorUnitario());
+                        System.out.println("IFCE: " + itemEmpresa.getItem() + ": " + itemEmpresa.getValorUnitario());
+                        System.out.println("--------------------------------------------------------------------");
+                    }
+                }
+            }
+        }
+    }
+
+}
