30726256970ac18f542e2cc7203fb0e982f1fc87
Cássio Tiago <101597036+slnntk@users.noreply.github.com>
2023-07-07 22:34:26 -0300

    .

--- src/a/Main.java
+++ src/a/Main.java
@@ -9,16 +9,19 @@ public class Main {
         Locale.setDefault(Locale.US);
 
         Leitor a = new Leitor();
-        List<Services> listEmp = a.lerArchiveEmp("D:\\Users\\cassi\\Downloads-1\\tsv\\EMP2.tsv");
-        List<Services> listMLG = a.lerArchiveMlg("D:\\Users\\cassi\\Downloads-1\\tsv\\MLG2.tsv");
+        List<Services> listEmp = a.lerArchiveEmp("D:\\Users\\cassi\\Downloads-1\\tsv\\PLANILHA - ORIGINAL - GRUPO 04.tsv");
+        List<Services> listMLG = a.lerArchiveMlg("D:\\Users\\cassi\\Downloads-1\\tsv\\PLANILHA - LICITAÇÃO IFCE - ATUALIZADA 07072023 - GRUPO 04.tsv");
 
 
         try{
             for (int i = 0;i < listEmp.size();i++){
-                if (listMLG.get(i).getValorUnitario() > listEmp.get(i).getValorUnitario()){
-                    System.out.println("MLG: " + listMLG.get(i).getItem() + ": " + listMLG.get(i).getValorUnitario());
-                    System.out.println("IFCE: " + listEmp.get(i).getItem() + ": " + listEmp.get(i).getValorUnitario());
-                    System.out.println("--------------------------------------------------------------------");
+                System.out.println(listMLG.get(i).getItem() + "|" + listEmp.get(i).getItem());
+                if (listMLG.get(i).getItem().equals(listEmp.get(i).getValorUnitario())){
+                    if (listMLG.get(i).getValorUnitario() > listEmp.get(i).getValorUnitario()){
+                        System.out.println("MLG: " + listMLG.get(i).getItem() + ": " + listMLG.get(i).getValorUnitario());
+                        System.out.println("IFCE: " + listEmp.get(i).getItem() + ": " + listEmp.get(i).getValorUnitario());
+                        System.out.println("--------------------------------------------------------------------");
+                    }
                 }
             }
         } catch (IndexOutOfBoundsException e){
