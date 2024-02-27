4f2e10c299129e51e37165dc9302f264f4538beb
Cássio Tiago <101597036+slnntk@users.noreply.github.com>
2024-02-27 14:26:46 -0300

    x

--- src/CANA/src/InsertionSort.java
+++ src/CANA/src/InsertionSort.java
@@ -11,7 +11,7 @@ public class InsertionSort {
         System.out.println();
 
         for (int j = 1;j < array.length;j++){
-            int key =  array[j];
+            int key = array[j];
             int i = j - 1;
             while (i > 0 && array[i] > key){
                 array[i + 1] = array[i];
